65990a96faccbed4e565bcd88e56de78a79c1464
Foysal Mohammad <foysal@earlblue.com>
2021-07-16 22:32:29 +0100

    rendered title in the browser after fetching

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,10 +1,26 @@
-import React from 'react';
-
+import React, { useState, useEffect } from 'react'
 
 export const Homepage = () => {
-    // https://newsapi.org/v2/everything?apiKey=f369c719bbeb4606af1a397ae1c004ea&q=Apple&from=2021-07-16&sortBy=popularity&page=1&pageSize=10
-    
-    return (<>
-        <h1>Homepage</h1>
-    </>);
+  const [titles, setTitles] = useState([])
+
+  console.log(titles)
+
+  useEffect(() => {
+    fetch(
+      'https://newsapi.org/v2/everything?apiKey=f369c719bbeb4606af1a397ae1c004ea&q=Apple&from=2021-07-16&sortBy=popularity&page=1&pageSize=10',
+    )
+      .then((res) => res.json())
+      .then((data) => setTitles(data.articles))
+      .catch((err) => {
+        console.log(err)
+      })
+  }, [])
+
+  return (
+    <h1>
+      {titles.map((title, index) => (
+        <p key={index}>{title.title}</p>
+      ))}
+    </h1>
+  )
 }
