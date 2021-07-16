c64b3f61368d8c0d4e1cd6fd975b8a7d89094c10
Emil Lozev <e.lozev@reply.com>
2021-07-16 13:12:49 +0100

    init commit

---
+++ src/Homepage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+export const Homepage = () => {
+    // https://newsapi.org/v2/everything?apiKey=f369c719bbeb4606af1a397ae1c004ea&q=Apple&from=2021-07-16&sortBy=popularity&page=1&pageSize=10
+    
+    return (<>
+        <h1>Homepage</h1>
+    </>);
+}
